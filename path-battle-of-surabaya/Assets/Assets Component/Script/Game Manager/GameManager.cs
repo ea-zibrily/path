a75db0d4aef44517d47d6289c0f1fa5d1b593f55
Zibril Yuan <zyuuannn@gmail.com>
2023-03-24 13:23:56 +0700

    arc prologue #1.2
    - optimisasi animasi

---
+++ path-battle-of-surabaya/Assets/Assets Component/Script/Game Manager/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    public const string NEXT_LEVEL = "NextLevel";
+    public const string GAME_OVER = "GameOver";
+
+    public void SceneController(string gameCondition)
+    {
+        switch (gameCondition)
+        {
+            case NEXT_LEVEL:
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                break;
+            case GAME_OVER:
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                break;
+            default:
+                Debug.LogWarning("Game Condition Not Found");
+                break;
+        }
+    }
+}
